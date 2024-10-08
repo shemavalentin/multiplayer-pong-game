cc5f1d811ce4c37e822b84efc6b040b063810cca
Shema N. Valentin <shemavalentin@gmail.com>
2024-10-08 16:44:19 +0200

    Identifying connected Clients and Listening for Events int the PongServer

--- server.js
+++ server.js
@@ -10,7 +10,28 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT} ...`);
 
+// Keeping track of how many players have told us they're ready, so we know we have
+// enough players for a game.
+
+let readyPlayerCount = 0;
+
 // Creating an event emitter that our socket server uses to register an event lister
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  //to have the same id on the server and on the client side we have to add socket.id.
+  // the only diference being that on the server side, we have multiple sockets being
+  // connected at the same time.
+  console.log("a user connected", socket.id);
+
+  // Listening to Ready event before the server starts the game
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+
+    // Increament by 1 every time a player sends us a READY event
+    readyPlayerCount++;
+
+    // Checking if the number of players are two
+    if (readyPlayerCount === 2) {
+      // We need to broadcast to players that the game starts, then starts the game
+    }
+  });
 });
